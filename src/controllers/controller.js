57cd0cc4f390bb9bc5acf576fea0fe666ededbf7
Sam <samme.s90@gmail.com>
2025-01-21 18:59:18 +0100

    update: controller

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,4 +1,4 @@
-import { RoomModel } from '../models/model.js'
+import { RoomModel } from "../models/model.js"
 
 export class Controller {
   hello(req, res, next) {
@@ -9,32 +9,31 @@ export class Controller {
     }
   }
 
-  async getRooms (req, res, next) {
+  async getRooms(req, res, next) {
     try {
-      const rooms = (await RoomModel.find())
-          .map(room => room.toObject())
+      const rooms = (await RoomModel.find()).map((room) => room.toObject())
 
-      res
-        .status(200)
-        .json({
-          rooms
-        })
+      res.status(200).json({
+        rooms
+      })
     } catch (err) {
       next(err)
     }
   }
 
-  async getRoom (req, res, next) {
+  async getRoom(req, res, next) {
     try {
-      const roomName = req.body.room
-      const rooms = (await RoomModel.find( {name: roomName}))
-          .map(room => room.toObject())
+      let roomName = null
+      if (req.param?.room) {
+        roomName = req.param.room
+      } else if (req.body?.room) {
+        roomName = req.body.room
+      }
+      const rooms = (await RoomModel.find({ name: roomName })).map((room) => room.toObject())
 
-      res
-        .status(200)
-        .json({
-          rooms
-        })
+      res.status(200).json({
+        rooms
+      })
     } catch (err) {
       next(err)
     }
