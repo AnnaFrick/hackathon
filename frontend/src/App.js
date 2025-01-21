52628416ea815eed4e2bb60c6b31a77903aa6c1b
Nelly <no222hh@student.lnu.se>
2025-01-21 19:26:36 +0100

    confirmation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Home';
 import Book from './Book';
+import Confirmation from './Confirmation';
 
 const App = () => {
     return (
@@ -9,6 +10,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/book" element={<Book />} />
+                <Route path="/confirmation" element={<Confirmation />} />
             </Routes>
         </Router>
     );
